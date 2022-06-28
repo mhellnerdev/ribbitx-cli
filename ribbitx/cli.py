a64f109f338a0a04ef62ce556a3219f2591a5863
Mark Hellner <mhellnerdev@gmail.com>
2022-06-28 01:05:35 -0400

    add comments

--- ribbitx/cli.py
+++ ribbitx/cli.py
@@ -1,24 +1,30 @@
 import requests
 import os
-import subprocess
 import click
 
 
+# base api url
 base_uri = "https://ribbitx.jfrog.io/artifactory/api"
+
+# api key. with this method, the api key must be exported to your local OS environment variables.
 api_key = os.environ.get('JFROG_API_KEY')
+
+# headers variable that is passed into the request string
 headers = {"X-JFrog-Art-Api": api_key}
 
-
+# required to group below extensible commands
 @click.group()
 def cli():
   pass
 
+# ping command to check HTTP status of hosted instance
 @click.command()
 def ping():
   click.echo('Pinging.... Response 200 means OK!')
   ping_response = requests.get(f"{base_uri}/system/ping", headers=headers)
   print(ping_response)
 
+# version command to check version of hosted instance
 @click.command()
 def version():
   click.echo('Getting remote instance version...')
@@ -26,12 +32,14 @@ def version():
   version_json = version_response.json()
   print("Artifactory Version: " + version_json['version'])
 
+# storage command to check status of hosted instance storage
 @click.command()
 def storage():
   storage_reponse = requests.get(f"{base_uri}/storageinfo", headers=headers)
   storage_json = storage_reponse.json()
   print(storage_json['fileStoreSummary'])
 
+# listrepos command to check list of available repos on the hosted instance
 @click.command()
 def listrepos():
   repo_type = input("What type of repositories would you like listed, REMOTE, LOCAL, or VIRTUAL? ")
