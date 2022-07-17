7f5380f1808aaff3ae4c1eb8dfefa1cae26b5196
Mark Hellner <mhellnerdev@gmail.com>
2022-07-16 21:10:32 -0400

    updated version in setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def read_requirements():
 # default setup.py function to define package and console script
 setup(
   name='ribbitx',
-  version='0.1',
+  version='0.2',
   packages=find_packages(),
   include_package_data=True,
   install_requires=read_requirements(),
